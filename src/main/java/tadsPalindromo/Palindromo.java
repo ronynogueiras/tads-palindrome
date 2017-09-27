11f80d25f86ef4a2ffdaa96f8de56d235af6d99b
Rony Nogueira <ronynogueiras.dev@gmail.com>
2017-09-27 00:12:55 -0300

    correção validação de palindromo

--- src/main/java/tadsPalindromo/Palindromo.java
+++ src/main/java/tadsPalindromo/Palindromo.java
@@ -9,7 +9,7 @@ public class Palindromo {
 	public boolean ehPalindromo(String frase) {
 		String fraseFiltrada = frase.toUpperCase().replace(" ", "").replace("-", "");
 	    for(int i = 0; i < fraseFiltrada.length(); i++) {
-	      if(fraseFiltrada.charAt(i) != fraseFiltrada.charAt(fraseFiltrada.length() - i)) {
+	      if(fraseFiltrada.charAt(i) != fraseFiltrada.charAt(fraseFiltrada.length() - (++i))) {
 	        return false;
 	      }
 	    }
